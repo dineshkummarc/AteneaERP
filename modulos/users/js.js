38b3592bde7290b2eaa1852d1cea6256bc0b817f
PanuWeb <panuweb@yahoo.com.ar>
2012-02-19 19:29:13 -0300

    Eliminando archivos innecesarios

--- modulos/users/js.js
+++
@@ -1,8 +0,0 @@
-$('#add_user button').click(function(){
-	datos  =         $('#add_user input#user').val();
-	datos += '&&&' + $('#add_user input#pass').val();
-	datos += '&&&' + $('#add_user select#grupos').val();
-	datos += '&&&' + $('#add_user input#options').val();
-	datos += '&&&' + $('#add_user select#admin').val();
-	alert(datos)
-})
